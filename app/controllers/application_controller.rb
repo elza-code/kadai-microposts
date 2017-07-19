7365c6bab1683d4c3dee26b79484b6704b162ea9
Ryuya <dragon@gmail.com>
2017-07-19 07:58:14 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  #[helper]をcontrollerで呼び出せるようにするためのコード
+  
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
